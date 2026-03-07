3e03fcbb8fe2a6885338923b6b4e84b855499415
RodrigoG <rodrigogonzalez.career@gmail.com>
2026-03-07 19:12:19 +0100

    Blank screen and game loop

--- main.py
+++ main.py
@@ -10,3 +10,16 @@ window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGTH))
 pygame.display.set_caption("Pong Game")
 
 clock = pygame.time.Clock()
+
+
+# GAME LOOP
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+            break
+
+    # Update Display
+    pygame.display.update()
+    clock.tick(60) # run at 60fps
